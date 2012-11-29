701f073d1e7d3b30dd9e72e95b542c8137b04f7b
joby890 <mcjoby890@gmail.com>
2012-11-29 13:53:01 -0800

    First
    Added Main.java

---
+++ SignCommand/src/com/admixhosting/plugins/Main.java
@@ -0,0 +1,41 @@
+package com.admixhosting.plugins;
+
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	public void onDisable() {
+	}
+	@EventHandler
+	public void OnSignClick(PlayerInteractEvent e) {
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (e.getClickedBlock().getType() == Material.SIGN || e.getClickedBlock().getType() == Material.WALL_SIGN || e.getClickedBlock().getType() == Material.SIGN_POST) {
+				Sign s = (Sign) e.getClickedBlock().getState();
+				if(s.getLine(0).equalsIgnoreCase("[command]")) {
+					if(e.getPlayer().hasPermission(s.getLine(1))) {
+						e.getPlayer().performCommand(s.getLine(1));
+					}
+				}
+			}
+		}
+	}
+	@EventHandler
+	public void OnSignPlace(SignChangeEvent e) {
+		if (e.getLine(0).equalsIgnoreCase("[command]")) {
+			if (!e.getPlayer().hasPermission("signcommand.create")|| !e.getPlayer().isOp()) {
+				e.setCancelled(true);
+			}
+			
+		}
+	}
+
+}
